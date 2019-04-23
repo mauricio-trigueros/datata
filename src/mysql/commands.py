234d2328ec8def1c21aea4bb5ca53d8d22c336c1
Mauricio Trigueros Garabito <mauricio.trigueros@cabonline.com>
2019-04-23 16:27:40 +0200

    escaping espaces when dumping mysql

--- src/mysql/commands.py
+++ src/mysql/commands.py
@@ -1,10 +1,12 @@
 import os
+from shlex import quote
 from datetime import datetime
 
 def full_backup(settings):
     print ("  Full database backup")
     timeNow = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
-    backupFileIso = "{}/mysql_full_backup_{}.sql".format(settings['local'],timeNow)
+    # The path can contain spaces, so we escape them with "quote"
+    backupFileIso = quote("{}/mysql_full_backup_{}.sql".format(settings['local'],timeNow))
     #print (settings)
     dumpDbCommand = "mysqldump --single-transaction --user={} --password={} --host={} --port={} {} > {}".format(
         settings['mysql-user'],
