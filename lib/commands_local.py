597e0d56bf05c260f2c4e6b311a04ba83a783988
Mauricio Trigueros <mauricio.trigueros@cabonline.com>
2020-04-13 19:42:27 +0200

    refactoring with execute_local_command

--- lib/commands_local.py
+++ lib/commands_local.py
@@ -5,35 +5,43 @@ import subprocess
 def get_file_size(path):
 	return os.path.getsize(path)
 
-def validate_local_folder_or_die(local_path):
-    if not os.path.exists(local_path):
-        sys.exit("Local path {} do not exist".format(local_path))
+def validate_local_folder_or_die(path):
+    if not os.path.exists(path):
+        sys.exit("Local path {} do not exist".format(path))
     else:
-        return local_path
+        return path
 
 # If we are creating file with 2019/03/moon.jpeg, folder "2019/03" should exist
-def verify_and_create_folder_path(local_path):
-	os.popen("mkdir -p '{}'".format(os.path.dirname(local_path)))
+def verify_and_create_folder_path(path):
+	os.popen("mkdir -p '{}'".format(os.path.dirname(path)))
 
-def is_valid_local_file(local_path):
-    return os.path.isfile(local_path) and os.path.getsize(local_path) > 0
+def is_valid_local_file(path):
+    return os.path.isfile(path) and os.path.getsize(path) > 0
 
-def print_size_reduction(original_path, dest_path):
-    reduction = int ((float (get_file_size(dest_path)) / float (get_file_size(original_path))) * 100)
+def print_size_reduction(origin_file_path, dest_file_path):
+    reduction = int ((float (get_file_size(dest_file_path)) / float (get_file_size(origin_file_path))) * 100)
     print ("--result-size-{}%".format(reduction))
 
-# Compress local JPG file.
-def compress_local_jpg(local_path, local_dest, dry_run):
-	print ("Compressing '{}' ...".format(local_path), end=' ')
-	verify_and_create_folder_path(local_dest)
-	command = "jpegoptim --strip-all --all-progressive --max=80 --quiet --preserve --stdout '{}' > '{}'".format(local_path, local_dest)	
+# Run a command that takes origin_file_path and produces dest_file_path (if dry_run is false)
+def execute_local_command(command, origin_file_path, dest_file_path, dry_run, size_reduction=False):
+	print(" Running '{}' on '{}'...".format(command.split(' ')[0], origin_file_path), end=' ')
+	verify_and_create_folder_path(dest_file_path)
 	if dry_run:
 		print("--DRY-RUN")
-		return	
+		return
 	os.system(command)
 	# Verify that file exist
-	if(is_valid_local_file(local_path)):
+	if(is_valid_local_file(dest_file_path)):
 		print("--done", end=' ')
-		print_size_reduction(local_path, local_dest)
+		if size_reduction: print_size_reduction(origin_file_path, dest_file_path)
 	else:
 		print("--ERROR")
+
+# Compress local JPG file.
+def compress_local_jpg(origin_file_path, dest_file_path, dry_run):
+	com = "jpegoptim --strip-all --all-progressive --max=80 --quiet --preserve --stdout '{}' > '{}'".format(origin_file_path, dest_file_path)	
+	execute_local_command(com, origin_file_path, dest_file_path, dry_run, size_reduction=True)
+
+def compress_local_png(origin_file_path, dest_file_path, dry_run):
+	com = "pngquant --force --quality 40-90 --speed 1 --output '{}' '{}'".format(dest_file_path, origin_file_path)
+	execute_local_command(com, origin_file_path, dest_file_path, dry_run, size_reduction=True)
--- newdatata.py
+++ newdatata.py
@@ -5,14 +5,38 @@ from lib.iterators import local_md5_files_iterator
 from lib.comparators import compare_file_dicts
 from lib.commands_server import download_file_from_server
 from lib.commands_server import upload_file_to_server
-from lib.commands_local import is_valid_local_file, compress_local_jpg
+from lib.commands_local import is_valid_local_file, compress_local_jpg, compress_local_png
 
 from lib.actions import settings
-
 
 print ("   Reading parameters from terminal....")
 settings = settings()
 print (settings)
+
+# extension: file extension to filter files from settings['local-folder']
+# force: if output file exist, we do nothing (unless force is true)
+# command: should take as argument local_path, dest_path and dry-run
+def execute_local_command(extension, force, command):
+	print(" Loading {} files from {}".format(extension, settings['local-folder']))
+	# First load all the files (based on the extension) where we are going to apply command.
+	local_files = local_md5_files_iterator(settings['local-folder'], extension)
+	if force:
+		# If force, we do not care if the file exist or not, just iterate all the files and execute command
+		print(" Force command")
+		for re in local_files:
+			local_path = local_files[re].get('full_path')
+			local_dest = os.path.join(settings['local-dest'], local_files[re].get('relative_path'))
+			command(local_path, local_dest, settings['dry-run'])
+	else:
+		# If not force, we need to get files from output folder, and check (compare_file_dicts not using MD5)
+		# if the file exist or not. Then we apply command to output files that do not exist.
+		print("Not forcing")
+		dest_files = local_md5_files_iterator(settings['local-dest'], extension)
+		res = compare_file_dicts(local_files, dest_files, False, False)
+		for re in res:
+			local_path = re.get('full_path')
+			local_dest = os.path.join(settings['local-dest'], re.get('relative_path'))
+			command(local_path, local_dest, settings['dry-run'])
 
 if settings['action'] == 'download_from_server_to_local':
 	print(" Downloading content from server to local...")
@@ -29,22 +53,7 @@ elif settings['action'] == 'upload_from_local_to_server':
 	for re in res:
 		upload_file_to_server(re, settings['serv-folder'], settings['server_client'], settings['dry-run'])
 elif settings['action'] == 'compress_local_images':
-	# Get list of JPG files to process
-	local_files = local_md5_files_iterator(settings['local-folder'], 'jpg')
-	# If Force, then we need to work all the files, even if they exist
-	if settings['force']:
-		print(" Force compressing images")
-		for re in local_files:
-			local_path = local_files[re].get('full_path')
-			local_dest = os.path.join(settings['local-dest'], local_files[re].get('relative_path'))
-			compress_local_jpg(local_path, local_dest, settings['dry-run'])
-	else:
-		print(" Compressing images if do not exist")
-		dest_files = local_md5_files_iterator(settings['local-dest'], 'jpg')
-		res = compare_file_dicts(local_files, dest_files, False, False)
-		for re in res:
-			local_path = re.get('full_path')
-			local_dest = os.path.join(settings['local-dest'], re.get('relative_path'))
-			compress_local_jpg(local_path, local_dest, settings['dry-run'])
+	execute_local_command('jpg', settings['force'], compress_local_jpg)
+	execute_local_command('png', settings['force'], compress_local_png)
 else:
 	print("No action")
